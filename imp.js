fd3dc772a835afef1be9882059dc32b82fe9785f
Justin Sijbolts <partydoos@gmail.com>
2022-05-19 20:45:57 +0200

    Initial commit

---
+++ imp.js
@@ -0,0 +1,23 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $("#blah").attr("src", e.target.result);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function readURL2(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $("#blah2").attr("src", e.target.result);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
